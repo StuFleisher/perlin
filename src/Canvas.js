99f1672c6eba85b59b2ea5f8fdc85493b2187383
Stuart Fleisher <stufleisher@gmail.com>
2023-10-03 17:55:21 -0400

    Refactored drawing - troubleshooting random seed bug

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,10 +1,6 @@
 // import * as React from "react";
-import { useLayoutEffect, createRef } from "react";
+import { useLayoutEffect, useState, createRef } from "react";
 import P5 from "p5";
-
-// import Sketch from "react-p5";
-
-
 
 /** Draws a perlin line using curved interpolation
  *
@@ -15,7 +11,7 @@ function drawCurves(p5, settings) {
 
   p5.beginShape();
   for (let x = 0; x < settings.width; x += (settings.width / settings.gridSize)) {
-    p5.noiseSeed(1);
+    p5.noiseSeed(99);
     p5.noiseDetail(settings.octaves, settings.influence);
     const y = p5.noise(settings.offset) * settings.height;
 
@@ -31,33 +27,34 @@ function drawCurves(p5, settings) {
 }
 
 function Canvas({ settings }) {
+  console.log("rendering canvas")
+  // const [sketchInstance,setSketchInstance] = useState(null);
 
   const canvasContainer = createRef();
 
   const sketch = (p) => {
 
     p.setup = () => {
-
       p.createCanvas();
       p.resizeCanvas(Number(settings.width), Number(settings.height));
-      p.background(20, 40, 30);
-      p.noiseSeed(1);
     };
 
     p.draw = () => {
+      p.clear()
+      p.background(20, 40, 30);
       drawCurves(p, settings);
-
-      p.frameRate(1);
-
-      p.noLoop();
+      // p.noLoop();
     };
+
   };
-  const p = new P5(sketch);
 
   useLayoutEffect(() => {
-    const inst = new P5(sketch, canvasContainer.current)
-    return ()=>inst.remove();
+    console.log("use effect running")
+    let sketchInstance = new P5(sketch, canvasContainer.current)
+
+    return ()=> sketchInstance.remove()
   }, [canvasContainer,settings]);
+
 
   return (
     <div ref={canvasContainer}></div>
